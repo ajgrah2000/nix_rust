49f2ab7138578f461f576d463de921b8cf743f73
me <a@localhost>
2026-04-01 00:40:22 +1100

    Minor update to paths.

--- flake.nix
+++ flake.nix
@@ -6,27 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     rust_rustsega = {
-      url = "path:./rust_rustsega";
+      url = "path:rust_rustsega";
       flake = true;
     };
 
     rust_rusted_atari2600 = {
-      url = "path:./rust_rusted_atari2600";
+      url = "path:rust_rusted_atari2600";
       flake = true;
     };
   };
 
   outputs = { self, nixpkgs, flake-utils, rust_rustsega, rust_rusted_atari2600 }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
     flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
       {
         packages = {
-          rustsega-emscripten = rust_rustsega.packages.${system}.emscripten;
-          rustsega-windows = rust_rustsega.packages.${system}.windows;
-          rusted_atari2600-emscripten = rust_rusted_atari2600.packages.${system}.emscripten;
-          rusted_atari2600-windows = rust_rusted_atari2600.packages.${system}.windows;
+          rustsega-emscripten = rust_rustsega.outputs.packages.${system}.emscripten;
+          rustsega-windows = rust_rustsega.outputs.packages.${system}.windows;
+          rusted_atari2600-emscripten = rust_rusted_atari2600.outputs.packages.${system}.emscripten;
+          rusted_atari2600-windows = rust_rusted_atari2600.outputs.packages.${system}.windows;
 
           all = pkgs.symlinkJoin {
             name = "all-builds";
